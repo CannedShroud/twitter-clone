82c2ee8f7b583f7390d35fa600c1a9f82066bd26
CannedShroud <hypersync112@gmail.com>
2020-09-12 23:37:25 +0530

    Init Feed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+
 function App() {
   return (
     <div className="app">
       <Sidebar />
-      {/**FEED */}
+      <Feed />
       {/**WIDGETS */}
     </div>
   );
