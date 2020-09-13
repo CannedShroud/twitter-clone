c5265080d7cd8734ec1b61f514c498882548e44f
CannedShroud <hypersync112@gmail.com>
2020-09-13 14:55:43 +0530

    Build Widgets

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,35 @@
 import React from "react";
 import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import { Search } from "@material-ui/icons";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Here will be them Widgets</h2>
+      <div className="widgets__input">
+        <Search className="widgets__searchIcon" />
+        <input type="text" placeholder="Search Twitter" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <TwitterTweetEmbed tweetId={"1304409372311855105"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="mrwhosetheboss"
+          options={{ height: 400 }}
+          className="widgets__twitterTimelineEmbed"
+        />
+        <TwitterShareButton
+          url={"https://instagram.com/cannedshroud/"}
+          options={{
+            text: "Twitter with React JS! Follow =>",
+            via: "synchyper",
+          }}
+        />
+      </div>
     </div>
   );
 }
