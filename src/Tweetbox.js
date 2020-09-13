fb56d7dce5412657a56d8941b2e92825114b8a49
CannedShroud <hypersync112@gmail.com>
2020-09-13 16:37:53 +0530

    Implement post tweet

--- src/Tweetbox.js
+++ src/Tweetbox.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Tweetbox.css";
+import db from "./firebase";
 import { Avatar, Button } from "@material-ui/core";
 
 function Tweetbox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (event) => {
+    event.preventDefault();
+    db.collection("posts").add({
+      displayName: "Rafe Quazi",
+      username: "cleverquazi",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar: "https://i.imgur.com/23yg1IZ.jpg",
+    });
+    setTweetImage("");
+    setTweetMessage("");
+  };
+
   return (
     <div className="tweetbox">
       <form>
         <div className="tweetbox__input">
           <Avatar src="https://i.imgur.com/23yg1IZ.jpg" />
-          <input type="text" placeholder="What's happening?" />
+          <input
+            type="text"
+            placeholder="What's happening?"
+            value={tweetMessage}
+            onChange={(event) => setTweetMessage(event.target.value)}
+          />
         </div>
         <input
+          onChange={(event) => setTweetImage(event.target.value)}
           type="text"
           placeholder="Enter Image URL"
           className="tweetbox__imageInput"
+          value={tweetImage}
         />
-        <Button className="tweetbox__tweetButton">Tweet</Button>
+        <Button
+          className="tweetbox__tweetButton"
+          type="submit"
+          onClick={sendTweet}
+        >
+          Tweet
+        </Button>
       </form>
     </div>
   );
